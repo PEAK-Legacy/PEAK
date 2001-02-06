38ac5dffd0630c45b0df8caa35043cac8fdbae0d
PJ Eby <pje@telecommunity.com>
2001-02-06 03:45:28 +0000

    Added SOX (Simple Objects from XML) module and unit tests, plus the beginnings of the RootModel and UML packages.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@265 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/tests/__init__.py
+++ src/peak/tests/__init__.py
@@ -1,3 +1,18 @@
 """
-TransWarp!
+TransWarp master test suite package.  Use with unittest.py to run all
+tests, or use the suite() function in an individual module to run just
+those tests.
 """
+
+def suite():
+
+    from unittest import TestSuite
+    s = []
+    
+    import Aspects, SOX
+    
+    for t in Aspects, SOX:
+        s.append(t.suite())
+
+    return TestSuite(s)
+
