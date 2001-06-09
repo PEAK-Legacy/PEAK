d70d9a111d02c1239b645e75a01d85382091531e
PJ Eby <pje@telecommunity.com>
2001-06-09 16:37:38 +0000

    Re-organized modules to split up the huge Components.py file, while making it possible to do 'from TW import *' or 'import TW' to get all the best goodies from the kernel.  Reinstated "Aspect" as an empty catalyst that turns a class into a template.  Refined builder-replacement rules a bit, so default builders (ComponentBuilder and DefaultBuilder) have to explicitly defer to existing builders.  Reinstated some old tests which depended on Aspect-ness.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@281 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/__init__.py
+++ src/peak/__init__.py
@@ -1,3 +1,36 @@
+"""The TransWarp Software Automation Framework
+
+The base package imports all the "kernel" objects and methods, so that you
+don't have to refer to things like 'TW.Interfaces.IBuilder' or
+'TW.Components.build'.  Instead, 'TW.build' or 'from TW import build' suffices.
+
+The current kernel modules are 'TW.Interfaces', 'TW.Components', and
+'TW.Builders' - any public item available directly from them is available here.
 """
-TransWarp!
-"""
+
+
+# Version-independent support for '__all__' in modules
+
+import sys
+
+if sys.version<'2.1':
+
+    def importAllFrom(name):
+        try:
+            exec "from %s import __all__" % name
+        except:
+            __all__ = ['*']
+
+        import string
+        exec "from %s import %s" % (name,string.join(__all__,",")) in globals()
+
+else:
+    def importAllFrom(name):
+        exec "from %s import *" % name in globals()
+
+
+importAllFrom('TW.Interfaces')
+importAllFrom('TW.Components')
+importAllFrom('TW.Builders')
+
+del sys
