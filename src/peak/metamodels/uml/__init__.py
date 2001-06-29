e693abc9f1771b8f9c614ba929e0ca9c9e90e739
PJ Eby <pje@telecommunity.com>
2001-06-29 20:25:52 +0000

    Moved UMLRecipe to UML package, for easier use by those experimenting with the current UML/XMI support.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@285 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/metamodels/uml/__init__.py
+++ src/peak/metamodels/uml/__init__.py
@@ -1,3 +1,15 @@
 """
-TransWarp!
+UML Models
 """
+
+from TW import Recipe, Import
+
+UMLRecipe = Recipe(
+    Import('TW.UML.Model:MetaModel'),
+    Import('TW.UML.Model:UMLModel'),
+    Import('TW.StructuralModel.Features:StructuralFeatures'),
+    Import('TW.StructuralModel.InMemory:InMemory'),
+    Import('TW.StructuralModel.Queries:ModelQuerying'),
+    Import('TW.XMI.Reading:XMIReading'),   
+)
+
