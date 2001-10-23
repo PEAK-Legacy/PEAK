5a239e6d38ffcba1fd5ccb7f83926c1e792ffa56
PJ Eby <pje@telecommunity.com>
2001-10-23 19:20:58 +0000

    Refined top-level basic structural model so it implements ISEF, and made it available from TW.SEF as an Import() object.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@295 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/metamodels/uml/__init__.py
+++ src/peak/metamodels/uml/__init__.py
@@ -2,14 +2,13 @@
 UML Models
 """
 
-from TW import Recipe, Import
+from TW import Recipe, Import, SEF
 
 UMLRecipe = Recipe(
     Import('TW.UML.Model:MetaModel'),
     Import('TW.UML.Model:UMLModel'),
 
-    Import('TW.StructuralModel.Basic:StructuralFeatures'),
-    Import('TW.StructuralModel.Basic:AcquisitionModel'),
+    SEF.AcquisitionModel,
 
     Import('TW.StructuralModel.Queries:ModelQuerying'),
     Import('TW.XMI.Reading:XMIReading'),   
