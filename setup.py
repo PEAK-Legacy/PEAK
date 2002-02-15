a15f061d5366da2d937e0de332c8bb26004a4235
PJ Eby <pje@telecommunity.com>
2002-02-15 14:22:07 +0000

    Initial (untested) setup.py
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@325 571e12c6-e1fa-0310-aee7-ff1267fa46bd

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="TransWarp",
+    version="0.2pre1",
+    description="The TransWarp Software Automation Toolkit",
+    author="Phillip J. Eby",
+    author_email="transwarp@eby-sarna.com",
+    url="http://www.zope.org/Members/pje/Wikis/TransWarp",
+    packages=[
+        'TW', 'TW.API', 'TW.Database', 'TW.MOF', 'TW.StructuralModel', 'TW.UML',
+        'TW.Utils', 'TW.XMI', 'TW.tests', 'TW.tests.Database',
+        'TW.tests.StructuralModel',
+    ],
+    package_dir = {'':'src'},
+    data_files = [
+        ('TW/tests/StructuralModel', ['TW/tests/StructuralModel/MetaMeta.xml']),
+    ],
+)
+
+
