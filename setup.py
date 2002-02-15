5153829d75d5aec1c0035bb1d9fec16e568df165
PJ Eby <pje@telecommunity.com>
2002-02-15 16:33:57 +0000

    Fixed the %&$#(@%& distutils data file path problem, after much Googling and Use of The Source.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@328 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- setup.py
+++ setup.py
@@ -1,20 +1,38 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from distutils.command.install_data import install_data
+
+class install_data(install_data):
+
+    def finalize_options (self):
+        self.set_undefined_options('install',
+                                   ('install_purelib', 'install_dir'),
+                                   ('root', 'root'),
+                                   ('force', 'force'),
+                                  )
 
 setup(
+
     name="TransWarp",
     version="0.2pre1",
     description="The TransWarp Software Automation Toolkit",
+    
     author="Phillip J. Eby",
     author_email="transwarp@eby-sarna.com",
+    
     url="http://www.zope.org/Members/pje/Wikis/TransWarp",
+    
     packages=[
         'TW', 'TW.API', 'TW.Database', 'TW.MOF', 'TW.StructuralModel', 'TW.UML',
         'TW.Utils', 'TW.XMI', 'TW.tests', 'TW.tests.Database',
         'TW.tests.StructuralModel',
     ],
+    
     package_dir = {'':'src'},
+
+    cmdclass = {'install_data': install_data},
+    
     data_files = [
         ('TW/tests/StructuralModel', ['src/TW/tests/StructuralModel/MetaMeta.xml']),
     ],
