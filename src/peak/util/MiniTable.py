5df8128113e1ae179e3afa0d94abbda01415e963
PJ Eby <pje@telecommunity.com>
2002-02-28 22:27:39 +0000

    Added "MiniTable" utility class.  Mainly, this is to support creating unit tests for the DataModel framework, but it should also be handy for the same kinds of things that TinyTable is useful for in ZPatterns apps.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@339 571e12c6-e1fa-0310-aee7-ff1267fa46bd

---
+++ src/peak/util/MiniTable.py
@@ -0,0 +1,118 @@
+"""An even more primitive and lightweight data table than even TinyTables..."""
+
+from UserList import UserList
+from kjbuckets import *
+
+
+class Table(UserList):
+
+    """Table( ('c1Name', 'c2Name'...), [ (r1c1,r1c2...), (r2c1,r2c2...) ...] )
+
+        Tables are 'UserList' objects, so you can iterate over them, access
+        rows by row number, take slices, etc.  Each row is a 'kjbuckets.kjDict'
+        mapping object, with column names mapping to values.  Mass 'SELECT',
+        'UPDATE', and 'DELETE' operations are also available.  Note that since
+        rows are mutable, modifying rows in row slices or 'SELECT' slices will
+        modify the original table.  'DELETE', however, affects only the table
+        it is applied to.  See the 'INSERT_ROWS' method for an explanation of
+        the initial load format.
+    """
+
+    def __init__(self, colNames=(), rowList=(), rawData=None):
+    
+        UserList.__init__(self,rawData)
+
+        if rowList and colNames:
+            self.INSERT_ROWS(colNames, rowList)
+
+
+    def INSERT(self, **values):
+
+        """table.INSERT(field1=value1, field2=value2...)
+        
+            Insert a row with the supplied field values.  Affects only the
+            specific table it is called upon.
+        """
+
+        self.data.append(kjDict(values.items()))
+
+
+
+
+    def SELECT(self, **where):
+
+        """table.SELECT(field1=value1, field2=value2...) -> Table slice
+
+            'SELECT' returns a new table which is the subset of rows from the
+            original table which match the field values asserted by the keyword
+            arguments to 'SELECT'.  You can then iterate over the new table, or
+            perform an 'UPDATE' on it to make changes.
+        """
+
+        where = kjDict(where.items()); matches = where.subset
+        return self.__class__(
+            rawData = [d for d in self.data if matches(d)]
+        )
+
+
+    def UPDATE(self, **setVals):
+
+        """table.UPDATE(setCol1=setVal1, setCol2=setVal2...)
+
+            Sets the specified column values for all rows in the table.
+            This is most useful in conjunction with 'SELECT', e.g.::
+
+                table.SELECT(foo=27).UPDATE(bar=50)
+
+            The above would set 'bar=50' on all rows of the original table
+            where the 'foo' value was equal to 27.  Note that updates
+            affect the value of rows in any tables that contain them, since
+            the row objects are shared between tables.
+        """
+
+        sets = setVals.items()
+        for d in self.data: map(d.add,sets)
+
+
+
+
+
+
+
+
+    def INSERT_ROWS(self, colNames, rowList):
+
+        """table.INSERT_ROWS(colnames tuple, rowlist)
+
+            Insert 'rowlist' of tuples, where 'colNames' is a tuple
+            containing the names of the columns used in the row tuples.
+            Example::
+
+                table.INSERT_ROWS(
+                    ('foo','bar','baz'), [
+                    (    1,    2,    3),
+                    (    4,    5,    6),
+                ])
+
+            The above is equivalent to::
+
+                table.INSERT(foo=1,bar=2,baz=3)
+                table.INSERT(foo=4,bar=5,baz=6)
+
+        """
+
+        self.data.extend( [kjDict(zip(colNames,row)) for row in rowList] )
+
+
+    def DELETE(self, **where):
+
+        """table.DELETE(field1=value1, field2=value2...)
+        
+            Delete rows which match the field values asserted in the keyword
+            arguments.  Affects only the specific table it is called upon.
+        """
+
+        where = kjDict(where.items())
+        matches = where.subset
+        self.data = [d for d in self.data if not matches(d)]
+
