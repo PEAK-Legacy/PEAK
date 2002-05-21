2ac8afb5d66281b1e13ab185fe1c7a0b34c3c44d
PJ Eby <pje@telecommunity.com>
2002-05-21 21:50:03 +0000

    Added 'getInitialContext()' API call to create an "initial context" object for use in naming operations, based on a configuration environment and keyword arguments.  Also, misc. cleanups to the management of the hidden "anonymous" default initial context used by the bare 'lookup()' and 'parseName()' APIs.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@389 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/naming/api.py
+++ src/peak/naming/api.py
@@ -1,32 +1,58 @@
 """Functions directly usable from the TW.Naming package"""
 
-__all__ = ['lookup','parseName']
+__all__ = ['getInitialContext', 'lookup', 'parseName']
+
+
+def getInitialContext(environ=None, **kw):
+
+    """Get an initial naming context, based on 'environ' and keyword args
+
+    If no environment or keyword arguments are supplied, a default
+    configuration from 'TW.Utils.AppConfig.getDefaultConfig()' is
+    used.  If an environment is supplied, it will be updated with any
+    supplied keyword arguments.  If only keyword arguments are supplied,
+    they will be used as the environment."""
+
+    if environ is None:
+        if kw:
+            environ, kw = kw, None
+        else:
+            from TW.Utils.AppConfig import getDefaultConfig
+            environ = getDefaultConfig()
+
+    if kw:
+        environ.update(kw)
+
+    return SPI.getInitialContext(environ)
 
 
 def lookup(name, requiredInterface=None):
-    """Look up 'name' and force result to 'requiredInterface'
-    """
-    return _context().lookup(name, requiredInterface)
+    """Look up 'name' in default context, returning 'requiredInterface'"""
+    return defaultInitialContext().lookup(name, requiredInterface)
 
 
 def parseName(name):
-    """Parse 'name' and return a name object appropriate to its scheme
-    """
-    return _context().parseName(name)
+    """Parse 'name' in default initial context, and return a name object"""
+    return defaultInitialContext().parseName(name)
+
+
+
 
 
 _initCtx = None
 
-def _context():
+def defaultInitialContext():
 
-    global _initCtx
+    """Return the default initial context used for simple lookups/parsing
+
+    Note: any changes made to the default initial context will affect all
+    lookups done with 'Naming.lookup()' and 'Naming.parseName()', so be
+    sure you know what you're doing if you do something with this!  That's
+    why this function isn't exported from the 'Naming.API' module."""
 
     if _initCtx is None:
-
-        from TW.Utils.AppConfig import getDefaultConfig
-        from SPI import getInitialContext
-
-        _initCtx = getInitialContext( getDefaultConfig() )
+        global _initCtx
+        _initCtx = getInitialContext()
 
     return _initCtx
 
