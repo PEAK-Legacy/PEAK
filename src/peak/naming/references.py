070c6ecee535022ea2a6c3f8341d6b20b4f18f9d
Ty Sarna <tsarna@sarna.org>
2002-06-15 21:29:32 +0000

    er, fix my fixing of CRLFs that wasn't much of a fix
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@418 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/naming/references.py
+++ src/peak/naming/references.py
@@ -1,120 +1,60 @@
 """References to Objects"""
 
-
-
 __all__ = ['Reference', 'LinkRef', 'RefAddr']
-
-
-
 
 
 class Reference(tuple):
 
-
-
     def __new__(klass, *refaddrs, **kw):
-
         ob = super(Reference,klass).__new__(klass,refaddrs)
-
         ob.__dict__.update(kw)
-
         return ob
 
-
-
     def __repr__(self):
-
         args = map(repr,self)
-
         args.extend(
-
             [('%s=%r' % i) for i in self.__dict__.items()]
-
         )
-
         return "Reference(%s)" % ','.join(args)
-
-
-
 
 
 class LinkRef(Reference):
 
-
-
     """Simple reference whose payload is 'RefAddr("LinkAddress",linkName)'"""
 
-
-
     def __new__(klass, linkName):
-
         return super(LinkRef,klass).__new__(
-
             klass, None,
-
             (RefAddr('LinkAddress',linkName),)
-
         )
 
-
-
     def linkName(self):
-
         """Retrieve the link name"""
-
         return self[0].content
-
-
 
     linkName = property(linkName)
 
-
-
     def __repr__(self):
-
         return "LinkRef(%s)" % `self.linkName`
-
-
-
-
 
 
 
 class RefAddr(object):
 
-
-
     __slots__ = ['type','content']
 
-
-
     def __init__(self,type,content):
-
         self.type, self.content = type, content
 
-
-
     def __eq__(self,other):
-
         try:
-
             return self.type==other.type and self.content==other.content
-
         except:
-
             return
 
-
-
     def __hash__(self):
-
         return hash((self.type,self.other))
 
-
-
     def __repr__(self):
-
         return "RefAddr(%r,%r)" % self.type, self.content
-
-
 
