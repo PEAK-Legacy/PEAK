3546cf9172a97193f3a98f4d1e70ec3d98479ead
Ty Sarna <tsarna@sarna.org>
2002-07-04 15:20:37 +0000

    Update to use peak.util.random
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@443 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/util/nodeid48.py
+++ src/peak/util/nodeid48.py
@@ -21,8 +21,10 @@ __all__ = ['getnodeid48']
 
 _hid48 = None
 
+
 r = ':'.join(["([0-9a-f]{2})"]*6)
 r = re.compile(r, re.IGNORECASE)
+
 
 def from_ifconfig():
     try:
@@ -39,84 +41,25 @@ def from_ifconfig():
     return None
 
 
-def from_6bytes(s):
-    try:
-        s = [ord(x) for x in s]
-        s[0] = s[0] | 128
-        s = ["%02x" % x for x in s]
-        s = ''.join(s)
-        return s.lower()
-    except:
-        pass
-        
-    return None
+def from_random():
+    from peak.util.random import randbytes
 
-       
-def from_devrandom():
-    try:
-        f = open('/dev/urandom')
-        s = f.read(6)
-        f.close()
-        if len(s) != 6:
-            return None
-
-        return from_6bytes(s)
-    except:
-        return None
-        
-    
-def from_fallback():
-    try:
-        import socket
-        hn = socket.gethostname()
-    except:
-        hn = ""
-
-    try:
-        p = os.getpid()
-    except:
-        p = 42
-                
-    try:
-        import whrandom
-        r = whrandom.random()
-    except:
-        r = 17 # the least random number
-
-    s = "%s-%s-%s-%s" % (p, time.time(), id(time), id(os))
-    s = "%s-%s-%s-%s-%s" % (s, sys.version, sys.byteorder, hn, hash(hash))
-    s = "%s-%s-%s-%s-%s" % (s, sys.getrefcount(None), id(None), id(r), id(p))
-    s = "%s-%s-%s-%s-%s" % (s, id(time.time()), id(s), id(hn), r)
-    
-    try:
-        import sha
-        s = sha.sha(s).digest()
-    except:
-        try:
-            import md5
-            s = md5.md5(s).digest()
-        except:
-            pass
-
-    return from_6bytes(s[:6])
+    s = randbytes(6)
+    s = [ord(x) for x in s]
+    s[0] = s[0] | 128
+    s = ["%02x" % x for x in s]
+    s = ''.join(s)
+    return s.lower()
 
 
 def from_win32():
     return None # XXX
 
 
-methods = [from_fallback]
+methods = [from_random]
 
-# Unixy
-#
-# The unix-dependand ways of finding an ID should fail gracefully if the
-# system is not supported, but the below contains sys.platform values
-# for which we shouldn't even bother trying.
-
-not_unixy_enough = ['win32']
-
-if posix is not None and sys.platform not in not_unixy_enough:
-    methods = [from_ifconfig, from_devrandom] + methods
+if posix is not None and sys.platform != 'win32':
+    methods = [from_ifconfig] + methods
 
 # Win32
 if sys.platform == 'win32':
