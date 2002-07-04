db2312f8dd8f2ef4229001704ffd79a2cb0be000
Ty Sarna <tsarna@sarna.org>
2002-07-04 15:29:06 +0000

    Universally Unique IDentifier type, including generation.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@445 571e12c6-e1fa-0310-aee7-ff1267fa46bd

---
+++ src/peak/util/uuid.py
@@ -0,0 +1,148 @@
+from nodeid48 import getnodeid48; id48 = getnodeid48()
+from peak.util.random import rand16, randbytes
+from time import time
+from md5 import md5
+
+lasttime = 0
+
+__all__ = [
+    'UUID',
+    'DNS_NS', 'URL_NS', 'OID_NS', 'X500_NS'
+]
+
+
+def getClockSeq():
+    return (rand16() & 0x3FFF) | 0x4000
+
+
+clock_seq = getClockSeq()
+
+
+class UUID(str):
+    __slots__ = []
+
+    def __new__(klass, from_string=None, name=None, ns=None, \
+      version=None, nodeid=None):
+        other_all_none = (name is None) and (ns is None) \
+            and (version is None) and (nodeid is None)
+        
+        global lasttime
+        
+        if from_string:
+            # Validate
+            
+            if not other_all_none:
+                raise ValueError, "If string is specified, nothing else may be"
+
+            from_string = from_string.lower()
+            ok = 0
+            parts = from_string.split('-')
+            if len(parts) == 5:
+                if [len(x) for x in parts] == [8, 4, 4, 4, 12]:
+                    try:
+                        # ensure it's all hex
+                        long(''.join(parts), 16)
+                        ok = 1
+                    except:
+                        pass
+               
+            if not ok:
+                raise ValueError, "Illegal syntax for UUID: " + from_string
+        elif other_all_none and 0: # just want a new id and can use win32
+            pass # use win32
+        else:
+            if version is None:
+                if name or ns:
+                    version = 3
+                else:
+                    version = 1
+
+            if version == 1:
+                if nodeid is None:
+                    nodeid = id48
+                else:
+                    nodeid = nodeid.lower()
+                    nodeid = ''.join(nodeid.split(':'))
+                    ok = 0
+                    if len(nodeid) == 12:
+                        try:
+                            long(nodeid, 16)
+                            ok = 1
+                        except:
+                            pass
+                    if not ok:
+                        raise ValueError, "Illegal node id: " + nodeid
+
+                # Delay if repeat. When Python and/or hardware becomes
+                # fast enough thatthis happens often (or ever!) we
+                # can come up with a faster non-delay algorithm.
+                
+                t = time()
+                while t == lasttime:
+                    t = time()
+                lasttime = t
+
+                ut = long(t * 10000000.0) + 0x01B21DD213814000L
+                ut = "%015x" % ut
+    
+                from_string = "%s-%s-1%s-%04x-%s" % (
+                    ut[7:], ut[3:7], ut[:3], clock_seq, nodeid
+                )
+
+            elif version == 3:
+                if not name or not ns:
+                    raise ValueError, "Both ns and name must be specified"
+
+                if type(ns) is not UUID:
+                    ns = UUID(ns)
+                    
+                h = md5(ns.bytes() + name).digest()
+
+            elif version == 4:
+                h = randbytes(16, prng=0, wait=1)
+                if h is None:
+                    raise OSError, "Crypto-quality entropy not available"
+
+            else:
+                raise ValueError, "Only versions 1, 3, and 4 supported"
+
+
+            if version == 3 or version == 4:
+                h = [ord(c) for c in h]
+
+                # insert the version
+                h[6] = (h[6] & 0x0f) | (version << 4)
+
+                # insert reserved bits
+                h[8] = (h[8] & 0x3f) | 0x40
+                
+                h = ''.join(['%02x' % c for c in h])
+                from_string = "%s-%s-%s-%s-%s" % (
+                    h[:8], h[8:12], h[12:16], h[16:20], h[20:])
+             
+        return super(UUID, klass).__new__(klass, from_string)
+
+
+    def bytes(self):
+        x = ''.join(self.split('-'))
+        return ''.join([chr(int(x[i:i+2], 16)) for i in range(0, len(x), 2)])
+
+
+    def __repr__(self):
+        return "UUID('%s')" % str(self)
+
+
+"""
+def genUUID():
+"""
+
+
+
+# per draft-leach-uuids-guids-01
+
+DNS_NS = UUID('6ba7b810-9dad-11d1-80b4-00c04fd430c8')
+URL_NS = UUID('6ba7b811-9dad-11d1-80b4-00c04fd430c8')
+OID_NS = UUID('6ba7b812-9dad-11d1-80b4-00c04fd430c8')
+X500_NS = UUID('6ba7b814-9dad-11d1-80b4-00c04fd430c8')
+
+NIL_UUID = UUID('00000000-0000-0000-0000-000000000000')
