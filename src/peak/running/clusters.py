9dd1eebd704afb4f9a9332d58ec4646ac39787d0
PJ Eby <pje@telecommunity.com>
2002-11-07 01:07:17 +0000

    More uniform cluster namespace.  The namespace under 'peak.running.cluster' now looks like this::
      .groups = all group names (except '__orphans__' and '__all__')
      .hosts  = all host names
    
      .groups.somegroup = hosts in somegroup
      .hosts.somehost   = groups somehost belongs to (including '__orphans__')
    
      .groups.__orphans__ = hosts without a group
      .groups.__all__     = all hosts, just like .hosts
    
      .hostname = this machine's hostname
      .shortname = short form of the preceding
    
    Docstring still needs a little work to cover all this properly.
    
    
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@619 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/running/clusters.py
+++ src/peak/running/clusters.py
@@ -13,8 +13,8 @@
  this, or for it to be useful.  Nor will anything break if you make use
  of these properties in your application and they haven't been set up.
  In the abscence of a cluster definition file, PEAK will behave as if
- the local hostname was listed in the cluster file (as a member of
- '_orphans' or '_all').
+ the local hostname was listed in the cluster file, without being part
+ of a group.
 
  Then environment variable 'CLUSTER' (or, in PEAK's case, the property
  'peak.running.cluster._filename', if it exists, else 'environ.CLUSTER')
@@ -40,82 +40,41 @@
     GROUP:qux
     frob.baz.com
     one.baz.com
-    LUMP:wierd
+    LUMP:weird
     qux
     prime
 
  PEAK treats this file as if it had seven groups.  The existance and
  memberships of the first four groups, 'odd', 'even', 'prime', and 'qux',
  should be clear.  A "lump" is a group defined in terms of other groups or
- lumps.  In this case the group 'wierd' contains the union of the groups
- qux and prime.  Note that one.baz.com is a member of both qux and prime. 
- The group "wierd" will contain it only once, however -- duplicates are
- removed automatically.  Another group, '_orphans', contains any hosts
+ lumps.  In this case the group 'weird' contains the union of the groups
+ 'qux' and 'prime'.  Note that one.baz.com is a member of both qux and prime. 
+ The group 'weird' will contain it only once, however -- duplicates are
+ removed automatically.  Another group, '__orphans__', contains any hosts
  that were listed at the beginning of the file before any 'GROUP:' lines. 
- Finally, the group '_all' contains the union of all hosts from all groups
- (including '_orphans').
 
- Each group is exported as a property 'peak.running.cluster.<groupname>'
- (for example, 'peak.running.cluster._all') with it's value a tuple of
+ Each group is exported as a property 'peak.running.cluster.groups.<groupname>'
+ (for example, 'peak.running.cluster.groups.prime') with it's value a tuple of
  strings of the hostnames of the members.
 
- The property 'peak.running.cluster._groups' will contain a
- tuple of strings naming each explicit group (that is, leaving out
- '_orphans' and '_all'). 
+ The property 'peak.running.cluster.groups' will contain a tuple of strings
+ naming each group except the '__orphans__' group.
 
- The property 'peak.running.cluster._host.<hostname>' will be a tuple of
- strings of the names of all groups the host belongs to (except '_all')
- For example, in the example above, 'peak.running.cluster._host.one.baz.com'
+ The property 'peak.running.cluster.hosts.<hostname>' will be a tuple of
+ strings of the names of all groups the host belongs to.  For example, in
+ the example above, 'peak.running.cluster.hosts.one.baz.com'
  is '("odd", "prime", "qux", "wierd")'.
 
- Finally, the property 'peak.running.cluster._hostname' is a string with
+ Finally, the property 'peak.running.cluster.hostname' is a string with
  the local machine's network hostname (per 'socket.gethostname()'), and
- 'peak.running.cluster._shortname' is the the same, truncated after the
+ 'peak.running.cluster.shortname' is the the same, truncated after the
  first '"."', if any. 
 """
 
 from peak.api import *
 import os
 
-
-
-
-def union(t1, t2):
-    d = {}
-    
-    for k in t1:
-        d[k] = 1
-    l = list(t1)
-    for k in t2:
-        if k not in d:
-            l.append(k)
-
-    return tuple(l)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+from kjbuckets import *
 
 
 
@@ -123,26 +82,34 @@ def union(t1, t2):
 
 def parseCluster(prefix, fn):
 
-    all = {}; gname = '_orphans'; group = (); groups = []
-    props = {prefix + gname : group}
-    lump = 0
-    
+    props = {}
+
     try:
         import socket
         hn =socket.gethostname()
+
     except:
         hn = 'NO_NAME'
 
-    props[prefix + '_hostname'] = hn
-    props[prefix + '_shortname'] = hn.split('.', 1)[0]
+    props[prefix + 'hostname'] = hn
+    props[prefix + 'shortname'] = hn.split('.', 1)[0]
 
     if fn is None or not os.path.exists(fn):
-        props[prefix + '_all'] = (hn,)
-        props[prefix + '_orphans'] = (hn,)
-        props[prefix + '_groups'] = ()
+        props[prefix + 'hosts'] = (hn,)
+        props[prefix + 'groups.__orphans__'] = (hn,)
+        props[prefix + 'groups'] = ()
         
         return props
-       
+
+
+    all    = kjGraph()
+    groups = kjSet()
+    hosts  = kjSet()
+
+    gname  = '__orphans__'
+    inLump = False
+
+
     for l in open(fn, 'r'):
 
         l = l.strip()
@@ -151,34 +118,49 @@ def parseCluster(prefix, fn):
             continue
 
         lumpline = l.startswith('LUMP:')
-        
+
+
+
         if lumpline or l.startswith('GROUP:'):
-            lump = lumpline
-            group = ()
+
+            inLump  = lumpline
             gname = l.split(':', 1)[1] 
-            props[prefix + gname] = group
-            groups.append(gname)
 
+            groups.add(gname)
 
-
-            
         else:
-            if lump:
-                na = props.get(prefix + l, ())
-                props[prefix + gname] = union(props[prefix + gname], na)
-                for h in na:
-                    all[h] = union(all[h], (gname,))
+            all.add(l, gname)
+
+            if inLump:
+                groups.add(l)
             else:
-                all.setdefault(l, []).append(gname)
-                props[prefix + gname] += l,
+                hosts.add(l)
 
-    for k, v in all.items():
-        props[prefix + '_host.' + k] = tuple(v)
 
-    props[prefix + '_groups'] = tuple(groups)
-    props[prefix + '_all'] = tuple(all.keys())
+    host_pre  = prefix+'hosts.'
+    group_pre = prefix+'groups.'
+
+    for host in hosts.values():
+        props[host_pre + host]   = tuple(all.reachable(host).values())
+
+
+    g = ~all    # get reverse mappping from groups to hosts
+
+    for group in groups.values() + ['__orphans__']:
+
+        props[group_pre + group] = tuple(
+            # don't include groups in groups' membership
+            (g.reachable(group) - groups).values()
+        )
+
+
+    props[prefix + 'groups']           = tuple(groups.values())
+    props[prefix + 'hosts']            = tuple(hosts.values())
+    props[prefix + 'groups.__all__']   = tuple(hosts.values())
 
     return props
+
+
 
 
 cache = {}
