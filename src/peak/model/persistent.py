c1b8a203d76f4f4f1a86dbbc92672d0e05a9eff4
PJ Eby <pje@telecommunity.com>
2002-12-10 01:56:18 +0000

    Split up 'peak.model.api' into 'peak.model.structural' and 'peak.model.persistent'.  This should make it possible to cleanly use peak.model facilities to build tools within PEAK itself, as long as they're essentially immutable datatypes.  I'm hoping this will be useful for redoing 'ParsedURL' as a 'model.DataType', but I've wanted to do this for a while anyway, because it was messy having virtually the entire 'peak.model' package being implemented in its 'api' module.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@797 571e12c6-e1fa-0310-aee7-ff1267fa46bd

---
+++ src/peak/model/persistent.py
@@ -0,0 +1,106 @@
+"""Persistent Elements"""
+
+from peak.api import *
+
+from structural import DataType
+from interfaces import *
+
+from Persistence import Persistent
+from Persistence.cPersistence import GHOST
+
+from peak.storage.lazy_loader import LazyLoader
+
+
+__all__ = [ 'Element' ]
+
+
+class ElementMeta(DataType.__class__, Persistent.__class__):
+    pass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class Element(DataType, Persistent):
+
+    """A (potentially persistent) domain element"""
+
+    __implements__ = IElement
+    __metaclass__  = ElementMeta
+    __setattr__    = binding.Base.__setattr__
+
+    def _setBinding(self,attr,value):
+        d = self.__dict__
+        
+        if d.get(attr) is not value or not isinstance(value,Persistent):
+            self._p_changed = True
+            d[attr]=value
+
+    def _getBinding(self,attr,default=None):
+
+        ob = self.__dict__.get(attr,default)
+
+        if isinstance(ob,LazyLoader):
+            del self.__dict__[attr]
+            ob.load(self,attr)
+            return self._getBinding(attr,default)
+
+        return ob
+
+    def _delBinding(self,attr):
+        if attr in self.__dict__:
+            self._p_changed = True
+            del self.__dict__[attr]
+
+    def setParentComponent(self, parentComponent, componentName=None):
+        if parentComponent is not None:
+            self._p_jar = parentComponent
+        self._p_oid = componentName
+
+    def getParentComponent(self):   return self._p_jar
+    def getComponentName(self):     return self._p_oid
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
