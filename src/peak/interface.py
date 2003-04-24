3c8ab92b69e39a9d1cbc5c2fcd4b9469eb570931
PJ Eby <pje@telecommunity.com>
2003-04-24 00:21:53 +0000

    Added a prototype PEP 246-like 'adapt()' implementation.  It incorporates some revisions/simplifications of PEP 246; see:
        http://mail.python.org/pipermail/python-dev/2003-April/034955.html
    
    for my proposal to the Python core developers list for how PEP 246 should
    be revised.
    
    
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@1016 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/interface.py
+++ src/peak/interface.py
@@ -4,9 +4,80 @@ from zope.interface import Interface, Attribute
 from zope.interface.implements import implements as addDeclarationToType
 
 __all__ = [
-    'Interface', 'Attribute', 
+    'Interface', 'Attribute', 'adapt',
 ]
 
 # We don't want to export 'addDeclarationToType' by default; we only use it
 # in 'peak.config.interfaces' right now, and that should go away eventually.
+
+
+# Monkeypatch 'Interface' to support the '__adapt__' side of the protocol
+# This is slower than just having the object check, but more
+# backward-compatible.
+
+def __adapt__(self, obj):
+    if self.isImplementedBy(obj):
+        return obj
+
+Interface.__class__.__adapt__ = __adapt__
+
+
+_marker = object()
+from sys import _getframe, exc_info
+
+from types import ClassType
+ClassTypes = ClassType, type
+
+
+
+
+
+
+
+
+
+
+
+
+def adapt(obj, protocol, default=_marker):
+
+    """PEP 246-alike: Adapt 'obj' to 'protocol', return 'default'
+
+    If 'default' is not supplied and no implementation is found,
+    raises 'TypeError'."""
+
+    if isinstance(protocol,ClassTypes) and isinstance(obj,protocol):
+        return obj
+        
+    try:
+        _conform = obj.__conform__
+    except AttributeError:
+        pass
+    else:
+        try:
+            result = _conform(protocol)
+            if result is not None:
+                return result
+        except TypeError:
+            if exc_info()[2].tb_frame is not _getframe():
+                raise
+
+    try:
+        _adapt = protocol.__adapt__
+    except AttributeError:
+        pass
+    else:
+        try:
+            result = _adapt(obj)
+            if result is not None:
+                return result   
+        except TypeError:
+            if exc_info()[2].tb_frame is not _getframe():
+                raise
+            
+    if default is _marker:
+        raise NotImplementedError("Can't adapt", obj, protocol)
+
+    return default
+
 
