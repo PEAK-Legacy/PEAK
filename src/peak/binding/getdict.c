3b71715b1955ef549e73f13252e08adada6c87ea
PJ Eby <pje@telecommunity.com>
2003-04-30 23:37:29 +0000

    Fixed C implementation of Once to work properly with "classic" classes.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@1057 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/binding/getdict.c
+++ src/peak/binding/getdict.c
@@ -1,20 +1,29 @@
 #include "Python.h"
 #include "object.h"
 
-PyObject * 
+PyObject *
 GET_DICTIONARY(PyObject *ob) {
 
         PyObject **dictptr = _PyObject_GetDictPtr(ob);
-        PyObject *dict;
+        PyObject *dict = NULL;
 
         if (dictptr == NULL) {
+
+            if (PyInstance_Check(ob))
+		dict = ((PyInstanceObject *)ob)->in_dict;
+
+            if (dict == NULL) {
                 PyErr_SetString(PyExc_AttributeError,
                                 "This object has no __dict__");
                 return NULL;
-        }
-        dict = *dictptr;
-        if (dict == NULL)
+            }
+
+        } else {
+            dict = *dictptr;
+            if (dict == NULL)
                 *dictptr = dict = PyDict_New();
+        }
+
         Py_XINCREF(dict);
         return dict;
 }
