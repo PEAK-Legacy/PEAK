32a3f88e7f691f8a51d68b728c9f7ae25bdbc307
PJ Eby <pje@telecommunity.com>
2003-06-18 21:12:24 +0000

    Changed 'import:' URLs to use a custom context, so they'll retrieve actual objects instead of addresses once we switch to adaptation-oriented lookups.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@1193 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/naming/factories/peak_imports.py
+++ src/peak/naming/factories/peak_imports.py
@@ -1,3 +1,4 @@
+from peak.api import *
 from peak.naming.api import *
 from peak.util.imports import importString
 
@@ -9,4 +10,18 @@ class importURL(URL.Base):
     def retrieve(self, refInfo, name, context, attrs=None):
         return importString(self.body)
 
+
+class importContext(naming.AddressContext):
+
+    schemeParser = importURL
+
+    def _get(self, name, retrieve=1):
+
+        if not name:
+            return self, None
+
+        try:
+            return importString(name.body), None
+        except ImportError:
+            return NOT_FOUND
 
