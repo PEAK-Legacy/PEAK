7d71b3357566601bac6fa3e46dbae531c3f59a84
PJ Eby <pje@telecommunity.com>
2003-07-19 21:29:53 +0000

    Added four new 'web.ITemplate*' interfaces, documenting the planned design of the template component framework.  We're almost ready to hack up a parser!
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@1271 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/web/interfaces.py
+++ src/peak/web/interfaces.py
@@ -8,6 +8,8 @@ __all__ = [
     'LOCATION_PROTOCOL', 'BEHAVIOR_PROTOCOL', 'INTERACTION_CLASS',
     'DEFAULT_METHOD', 'APPLICATION_LOG', 'AUTHENTICATION_SERVICE',
     'ERROR_PROTOCOL', 'SKIN_SERVICE', 'IWebException',
+    'ITemplateNode',    'ITemplateNodeFactory',
+    'ITemplateElement', 'ITemplateElementFactory',
 ]
 
 
@@ -35,8 +37,6 @@ else:
     zopePublicationInterfaces = (
         IPublication, IBrowserPublication, IXMLRPCPublication
     )
-
-
 
 
 class IWebInteraction(IInteraction):
@@ -115,6 +115,170 @@ class IWebException(Interface):
 
 
 
+
+
+
+
+
+
+class ITemplateNode(Interface):
+
+    """A component of a page template"""
+
+    def renderTo(interaction, writeFunc, currentModel, executionContext)
+        """Write template's output by calling 'writeFunc()' 0 or more times
+
+        'interaction' is the current 'IWebInteraction'.  'currentModel' is
+        the current "model" object being rendered (e.g. the object the template
+        is a method of).  'executionContext' is a component used to supply
+        arbitrary properties/utilities during template execution.  All of these
+        parameters should be supplied to executed child nodes as-is, unless
+        the current view wishes to change them.
+
+        For example, if a node wishes to add properties to the
+        'executionContext' for its children, it should create a new Component
+        with the old 'executionContext' as the new component's parent, then
+        supply the new component to child nodes as their 'executionContext'.
+        """
+
+    staticText = Attribute(
+        """The static XML text that represents this node, or None if
+        the node contains any dynamic content."""
+    )
+
+    # XXX should be some kind of parseInfo w/source file/line/column
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ITemplateNodeFactory(Interface):
+
+    """Factory to produce a literal or text node"""
+
+    def __call__(parentComponent, componentName=None, xml=u'',
+        # XXX parse info
+    ):
+        """Create an ITemplateNode w/specified XML text
+
+        'xml' will contain the text as it would appear in an XML document;
+        i.e., it is already XML-escaped, so no further processing is required
+        on output.
+
+        Note that only the first two arguments are positional; the 'xml'
+        argument must be supplied as a keyword."""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+class ITemplateElement(IWebMethod, ITemplateNode):
+
+    """A component representing an XML/HTML element"""
+
+    def addChild(node):
+        """Add 'node' (an 'ITemplateNode') to element's direct children"""
+
+    def addPattern(name, element):
+        """Declare 'element' (an 'ITemplateElement') as part of pattern 'name'
+
+        Note that 'element' is not necessarily a direct child of the current
+        element, and that this method may be called multiple times for the
+        same 'name', if multiple pattern nodes are declared with the same name.
+        It's up to the element to do any validation/restriction of pattern
+        names/values."""
+
+
+    tagFactory = Attribute(
+        """'ITemplateElementFactory' to be used for non-view child tags"""
+    )
+
+    textFactory = Attribute(
+        """'ITemplateNodeFactory' to be used for plain text child nodes"""
+    )
+
+    literalFactory = Attribute(
+        """'ITemplateNodeFactory' to be used for literals (e.g. comments,
+        processing instructions, etc.) within this element"""
+    )
+
+
+
+
+
+
+
+
+
+
+
+
+class ITemplateElementFactory(Interface):
+
+    """Produce an 'ITemplateElement' from a source document element"""
+
+    def __call__(parentComponent, componentName,
+        tagName=u'', attribItems=(), nonEmpty=False,
+        viewProperty=None, modelPath=None, patternName=None,
+        # XXX xmlns maps, parse info
+    ):
+        """Create an ITemplateElement w/specified attribs, etc.
+
+        'tagName' -- the element tag name, exactly as it appeared in the source
+        document
+
+        'attribItems' -- a list of '(name,value)' tuples representing the
+        source document attributes for this element, in the order they were
+        defined, and only the supplied attributes (i.e. no DTD-implied default
+        values).  Template markup attributes (view, model, pattern) are
+        excluded.
+
+        'nonEmpty' -- indicates that this element should always have an open
+        and close tag, even if it has no children.  This is to support HTML.
+
+        'viewProperty' -- the 'PropertyName' used to retrieve this factory
+        (minus the 'peak.web.views.' prefix), or None if the source document
+        didn't include a 'view' attribute.
+
+        'modelPath' -- a 'naming.CompoundName' representing the path specified
+        in the source document's 'model' attribute, or 'None' if not supplied.
+
+        'patternName' -- the value of the 'pattern' attribute in the source
+        document, or 'None' if not supplied.
+
+        Note that only the first two arguments are positional; all others must
+        be supplied as keywords."""
 
 
 
