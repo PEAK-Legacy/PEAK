907cedbfa0172f7b21e5709e7e832a2a2a814f07
Ty Sarna <tsarna@sarna.org>
2003-08-28 03:01:25 +0000

    add default paramaters to pushRLHistory() except for filename.
    Add a new parameter for the history file length limit in lines,
    defaulting to -1 (unlimited)
    
    Add a docstring for pushRLHistory()
    
    
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@1364 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/util/readline_stack.py
+++ src/peak/util/readline_stack.py
@@ -20,15 +20,38 @@ except:
 
 histstack = []
 curhist = curcompl = curdelims = None
+curlines = -1
 
 
-def pushRLHistory(fn, completer, delims, environ):
-    global curhist, curcompl, curdelims, histstack
+def pushRLHistory(fn, completer=None, delims=None, environ=None, lines=-1):
+    """Set up readline with given parameters, saving old state.
+    This function is safe to call if readline is absent; it will
+    silently do nothing.
+    
+    fn is a filename, relative to the home directory, for the history file.
+
+    completer is a completion callable (see readline module documentation),
+    or as a convenience, True may be passed for an interactive Python
+    completer.
+    
+    delimns is a string containing characters that delimit completion words.
+    passing None will use teh default set.
+    
+    environ is the environment in which to look up variables such as the
+    home directory. 
+    
+    lines is the number of lines of history to keep, or -1 for unlimited.
+    """
+    
+    global curhist, curcompl, curdelims, curlines, histstack
     
     if readline:
+        if environ is None:
+            environ = os.environ
+            
         if curhist:
             readline.write_history_file(curhist)
-            histstack.append((curhist, curcompl, curdelims))
+            histstack.append((curhist, curcompl, curdelims, curlines))
         homedir = environ.get('HOME', os.getcwd())
         curhist = os.path.join(homedir, fn)
         try:
@@ -43,24 +66,27 @@ def pushRLHistory(fn, completer, delims, environ):
 
         curcompl = completer
         curdelims = delims
+        curlines = lines
             
         readline.set_completer(completer)
         readline.set_completer_delims(curdelims)
+        readline.set_history_length(curlines)
 
 
 def popRLHistory():
-    global curhist, curcompl, curdelims, histstack
+    global curhist, curcompl, curdelims, curlines, histstack
 
     if readline:
         readline.write_history_file(curhist)
         if histstack:
-            curhist, curcompl, curdelims = histstack.pop()
+            curhist, curcompl, curdelims, curlines = histstack.pop()
             try:
                 readline.read_history_file(curhist)
             except:
                 pass
             readline.set_completer(curcompl)
             readline.set_completer_delims(curdelims)
+            readline.set_history_length(curlines)
         
 
 
