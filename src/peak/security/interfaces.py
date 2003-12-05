c7096349646a0ea2d9db8e5defca8137da9d0464
PJ Eby <pje@telecommunity.com>
2003-12-05 18:27:36 +0000

    Doc enhancements for peak.security interfaces.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@1531 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/security/interfaces.py
+++ src/peak/security/interfaces.py
@@ -25,6 +25,12 @@ class IAccessAttempt(Interface):
 
     subject = Attribute("""The object to which access is desired""")
 
+    name = Attribute(
+        """The name of the sub-object of 'subject' to which acess is desired
+
+        (May be 'None' if access to 'subject' itself is desired.)"""
+    )
+
     def allows(subject=NOT_GIVEN, name=NOT_GIVEN, permissionNeeded=NOT_GIVEN,
         user=NOT_GIVEN
     ):
@@ -33,14 +39,17 @@ class IAccessAttempt(Interface):
 
 
 
-
-
-
-
-
-
 class IAuthorizedPrincipal(Interface):
 
+    """A principal (e.g. user) w/global grants or denials of permission
+
+    In addition to permission checks based on business rules, it may be
+    desirable to globally grant or deny a permission for a given principal.
+    If this is the case, providing an adapter from your user object class
+    to 'IAuthorizedPrincipal' will allow the user object to participate
+    in permission checks via the 'checkGlobalPermission()' method.
+    """
+    
     def checkGlobalPermission(attempt):
         """Does principal have a global grant or deny of 'attempt.permission'?
 
@@ -51,9 +60,66 @@ class IAuthorizedPrincipal(Interface):
         Note that principals are not responsible for local grants/denials."""
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 class IInteraction(Interface):
 
-    """Component representing a security-controlled user/app interaction"""
+    """Component representing a security-controlled user/app interaction
+
+    An interaction provides the necessary context to identify what security
+    rules should be used, and on whose behalf the action is being performed
+    (i.e. the principal).  To determine if an access is allowed, you use the
+    interaction's 'allows()' method.
+
+    To determine what set of security rules are to be applied, an
+    interaction supplies a 'permissionProtocol' attribute, to which
+    abstract and concrete permissions will be adapted for checking.  In the
+    simple case, 'IPermissionChecker' is used as this 'permissionProtocol'.
+    However, if one application extends a library or another application that
+    provides default security rules registered under 'IPermissionChecker',
+    the new application may wish to set the 'permissionProtocol' to a
+    'protocols.Variation' of 'IPermissionChecker', in order to declare
+    new rules that take precedence over the defaults.  For example::
+
+        myRulesProtocol = protocols.Variation(IPermissionChecker)
+
+        class MyRuleSet(security.RuleSet):
+            # declare rules that override default rules
+
+        MyRuleSet.declareRulesFor(myRulesProtocol)
+        
+        anInteracton = security.Interaction(
+            parentComponent,
+            permissionProtocol = myRulesProtocol,
+            user = someUser
+        )
+        
+    The application could then check whether 'someUser' has permissions
+    for various objects, using the rules defined for 'myRulesProtocol'
+    (with fallback to any rules defined for 'IPermissionChecker').
+    
+    """
+
+
+
 
     user = Attribute("""The IPrincipal responsible for the interaction""")
 
@@ -85,13 +151,31 @@ class IPermissionChecker(Interface):
     """An object that can verify the presence of a permission"""
 
     def checkPermission(attempt):
-        """Does the principal for 'attempt' have permission 'permType'?
+        """Does the principal for 'attempt' have the needed permission?
 
         This method may return any false value to indicate failure, but
         returning a 'security.Denial()' is preferred."""
 
 
+
+
+
+
+
+# Permission interfaces
+
 class IConcretePermission(Interface):
+
+    """An abstract permission applied to a specific object type
+
+    For example, "View content of Document" or "Content Manager of Folder".
+    Concrete permissions allow separate security rules to be declared for
+    an abstract permission, based on the type of object being accessed.
+    The 'addRule' method of a concrete permission is used to define
+    security rules for concrete permissions.  (Note: abstract permissions
+    are actually concrete permissions themselves; they are roughly equivalent
+    to concrete permissons on the type 'object'.)
+    """
 
     def getAbstract():
         """Return an IAbstractPermission for this permission"""
@@ -107,8 +191,34 @@ class IConcretePermission(Interface):
         """Return a default security.Denial() to be used when a check fails"""
 
 
+
+
+
+
+
+
+
+
+
+
+
+
 class IAbstractPermission(IConcretePermission):
 
+    """A conceptual permission, group, or role
+
+    For example, "View content", or "Content Manager" would be "abstract
+    permissions".  The easiest way to create an abstract permission is to
+    subclass 'security.Permission', e.g.::
+
+        class ViewContent(security.Permission):
+            "Permission to view an object's content"
+
+    That's all that's needed, since abstract permissions are simply a marker
+    that denotes the *idea* of a particular permission.
+    
+    """
+    
     def of(protectedObjectType):
         """Return a subclass IConcretePermission for 'protectedObjectType'"""
 
@@ -120,6 +230,21 @@ class IAbstractPermission(IConcretePermission):
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Protected-Object interfaces
 
 class IGuardedObject(Interface):
 
@@ -147,8 +272,6 @@ class IGuardedDescriptor(Interface):
     permissionNeeded = Attribute(
         "Sequence of abstract permissions needed, or 'None' to keep default"
     )
-
-
 
 
 
