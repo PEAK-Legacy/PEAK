8e91a41a7bf4964a959f38e3909f24a1ed9008d8
PJ Eby <pje@telecommunity.com>
2004-01-19 18:56:28 +0000

    Fix 'invoke' program dumping core when run by itself w/out arguments
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@1612 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- scripts/invoke.c
+++ scripts/invoke.c
@@ -44,14 +44,12 @@ int main(int argc, char **argv) {
     char **useargs = argv;
     char **newargs;
     char *p;
-    int argct = argcount(argv[1]);
-
-    /* printf("argct=%d\n", argct); */
+    int argct;
 
     if ( argc>1 ) {
 
         /* printf("2 or more args\n"); */
-
+        argct = argcount(argv[1]);      /* printf("argct=%d\n", argct); */
         newargs = useargs = (char **)calloc(argct+argc+1, sizeof(char *));
         *newargs++ = argv[0];
 
@@ -73,10 +71,12 @@ int main(int argc, char **argv) {
             *newargs++ = *argv++;
 
         *newargs = NULL;
+
+        /* printf("doing exec\n"); */
+        execvp(useargs[1], useargs+1);
     }
-    /* printf("doing exec\n"); */
-    execvp(useargs[1], useargs+1);
     return 2; /* if we're still here, it's a usage error */
 }
+
 
 
