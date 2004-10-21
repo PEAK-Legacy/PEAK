9aaf072e348be63ab0d5cbf97fb88887189f64b7
PJ Eby <pje@telecommunity.com>
2004-10-21 00:49:25 +0000

    Sitemap parser, milestone 2.  We now have 'import', 'offer', 'require', 'container', and 'location', all of which are mostly implemented, and most of which are completely implemented.  :)  It's still not ready for prime time, but it at least now does some things that are interesting.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@1881 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/web/sitemaps.py
+++ src/peak/web/sitemaps.py
@@ -1,24 +1,84 @@
 from peak.api import *
 from interfaces import *
 from places import Location
+from peak.util.imports import lazyModule
+from peak.util import SOX
+
+def isRoot(data):
+    return 'previous' not in data
 
 def finishComponent(parser,data):
     if 'sm.component' in data:
         return data['sm.component']
 
+def getGlobals(data):
+    cur = data
+    while 'sm_globals' not in cur:
+        cur = cur['previous']
+    return data.setdefault('sm_globals',cur['sm_globals'].copy())
+
 def findComponentData(data):
-    prev = data['previous']   
+    prev = data['previous']
     while 'sm.component' not in prev:
         prev = prev.get('previous')
     return prev
-    
+
+def assertNotTop(parser,data):
+    if isRoot(data['previous']):
+        parser.err(
+            "%(name)r element cannot be top-level in the document" % data
+        )
+
+def evalObject(data,expr):
+    g = getGlobals(data['previous'])
+    return eval(expr,g,g)
+
+
+
+
+
+
+
+def acquirePermission(data,attrs):
+    if 'permission' in attrs:
+        perm = data['sm.permission'] = evalObject(data,attrs['permission'])
+        return perm
+    while data is not None and 'sm.permission' not in data:
+        data = data.get('previous')
+    if data is not None:
+        return data['sm.permission']
+    return security.Anybody
+
+
+locRequired = ()
+locOptional = 'name','class','id','permission',
+
 def startLocation(parser,data):
-    attrs = dict(data['attributes'])
+    attrs = SOX.validatedAttributes(parser,data,locRequired,locOptional)
+    acquirePermission(data,attrs)
     prev = findComponentData(data)
     parent = prev['sm.component']
-    data['sm.component'] = loc = Location(parent,attrs.get('name'))
+    name = attrs.get('name')
+    if isRoot(prev):
+        if name is not None:
+            parser.err("Root location cannot have a 'name'")
+    elif not name:
+        parser.err("Non-root locations must have a 'name'")
+
+    if 'class' in attrs:
+        loc = evalObject(data,attrs['class'])(parent,name)
+    else:
+        loc = Location(parent,name)
+
+    if 'id' in attrs:
+        loc.registerLocation(attrs['id'],'.')
+
+    data['sm.component'] = loc
     data['sm.sublocations'] = subloc = {}
     loc.addContainer(subloc)
+
+
+
 
 def defineLocation(parser,data):
     data['finish'] = finishComponent
@@ -29,11 +89,76 @@ def defineLocation(parser,data):
         data['sm.sublocations'][loc.getComponentName()] = loc
     data['child'] = addLocation
 
+
+def doImport(parser,data):
+    attrs = SOX.validatedAttributes(parser,data,('module',),('as',))
+    module = attrs['module']
+    as_ = attrs.get('as', module.split('.')[-1])
+    getGlobals(data['previous'])[as_] = lazyModule(module)
+
+def defineImport(parser,data):
+    assertNotTop(parser,data)
+    data['start'] = doImport
+    data['empty'] = True
+
+
+def doContainer(parser,data):
+    attrs = SOX.validatedAttributes(parser,data,('object',),('permission',))
+    prev = findComponentData(data)
+    perm = acquirePermission(data,attrs)
+    prev['sm.component'].addContainer(evalObject(data,attrs['object']),perm)
+
+def defineContainer(parser,data):
+    assertNotTop(parser,data)
+    data['start'] = doContainer
+    data['empty'] = True
+
+
+
+
+
+
+
+
+
+def doOffer(parser,data):
+    attrs = SOX.validatedAttributes(parser,data,('path','as',))
+    prev = findComponentData(data)
+    perm = acquirePermission(data,attrs)
+    prev['sm.component'].registerLocation(attrs['as'],attrs['path'])
+
+def defineOffer(parser,data):
+    assertNotTop(parser,data)
+    data['start'] = doOffer
+    data['empty'] = True
+
+
+def doRequire(parser,data):
+    attrs = SOX.validatedAttributes(parser,data,('permission',))    #, helper
+    acquirePermission(data,attrs)
+
+def defineRequire(parser,data):
+    assertNotTop(parser,data)
+    data['start'] = doRequire
+
+
 def setupDocument(parser,data):
 
     def setRoot(ob):
         data['sm.component'] = ob
+
     data['child'] = setRoot
     data['finish'] = finishComponent
     data['sm.component'] = data['parent']
-    
+
+
+
+
+
+
+
+
+
+
+
+
--- src/peak/web/tests/test_sitemap.py
+++ src/peak/web/tests/test_sitemap.py
@@ -7,7 +7,7 @@ class ParserTests(TestCase):
     def setUp(self):
         self.xml_parser = config.XMLParser(
             config.lookup(testRoot(), 'peak.web.sitemap_schema'),
-            parent = testRoot(),
+            parent = testRoot(), sm_globals=globals()
         )
         self.parse = self.xml_parser.parse
 
@@ -19,6 +19,9 @@ class ParserTests(TestCase):
         self.finish = nparser._afterParsing
         self.policy = web.TestPolicy(testRoot())
 
+    def traverse(self,start,name):
+        return self.policy.newContext(start=start).traverseName(name)
+
     def testRoot(self):
         self.startElement('location',[])
         v = self.endElement('location')
@@ -27,36 +30,137 @@ class ParserTests(TestCase):
 
     def testChild(self):
         self.startElement('location',[])
-        self.startElement('location',['name','foo'])    
+        self.startElement('location',['name','foo'])
         inner = self.endElement('location')
         self.failUnless(web.IPlace(inner) is inner)
         self.assertEqual(inner.place_url,'foo')
         outer = self.endElement('location')
         self.failUnless(inner.getParentComponent() is outer)
         self.assertEqual(outer.place_url,'')
-        ctx = self.policy.newContext(start=outer)
-        self.failUnless(ctx.traverseName('foo').current is inner)
-        
+        self.failUnless(self.traverse(outer,'foo').current is inner)
+
+    def testRootIsValid(self):
+        self.assertRaises(SyntaxError, self.startElement, 'nosuchthing',[])
+
+    def testRootIsNameless(self):
+        self.assertRaises(SyntaxError,
+            self.startElement, 'location',['name','foo'])
+
+    def testLocationClassAndGlobals(self):
+        self.startElement('location',['class','TestLocation'])
+        self.failUnless(isinstance(self.endElement('location'),TestLocation))
+        self.startElement('location',['class','TestLocation'])
+        self.startElement('location',['name','y'])
+        self.failIf(isinstance(self.endElement('location'),TestLocation))
+        self.failUnless(isinstance(self.endElement('location'),TestLocation))
+
+    def testImportElement(self):
+        self.startElement('location',[])
+        self.startElement('import',['module','peak.web.tests.test_sitemap'])
+        self.assertEqual(self.endElement('import'),None)
+        self.startElement('location',
+            ['class','test_sitemap.TestLocation','name','x'])
+        self.failUnless(isinstance(self.endElement('location'),TestLocation))
+
+    def testImportEmpty(self):
+        self.startElement('location',[])
+        self.startElement('import',['module','peak.web.tests.test_sitemap'])
+        self.assertRaises(SyntaxError, self.startElement, 'location',[])
+
+    def testRootIsLocation(self):
+        self.assertRaises(SyntaxError, self.startElement,
+            'import',['module','peak.web.tests.test_sitemap'])
+
+    def testInvalidAttrs(self):
+        self.assertRaises(SyntaxError,
+            self.startElement, 'location',['foo','bar'])
+
+    def testMissingAttrs(self):
+        self.startElement('location',[])
+        self.assertRaises(SyntaxError, self.startElement, 'import',[])
 
 
-    # Validate attrs - no name on root, no unrecognized non-ns attrs
-    # Validate elements - no unrecognized elements
-    # Fail if top element is not 'location'
-    # Root parser stack has 'sm.globals' (from peak.config.ini_files)
-    # Each location level copies previous location's sm.globals
-    # Import element adds lazyModule to globals
-    # Import element disallows children
-    # Container element evals immediately and adds to current location
-    # 'id' on location registers '.' for id
-    # 'offer' registers path
-    # 'offer' and 'container' fail if not direct child of location
+    def testChildLocationMustBeNamed(self):
+        self.startElement('location',[])
+        self.assertRaises(SyntaxError, self.startElement, 'location',[])
 
-    # content, require, allow, view...
-    # XXX Permissions should be per-location
+    def testContainer(self):
+        self.startElement('location',[])
+        self.startElement('location',['name','foo'])
+        self.startElement('container',['object','globals()'])
+        self.endElement()
+        loc = self.endElement()
+        self.failUnless(
+            self.traverse(loc,'TestLocation').current is TestLocation
+        )
 
-                
+        self.startElement('location',['name','bar'])
+        self.startElement('container',
+            ['object','globals()','permission','security.Nobody']
+        )
+        self.endElement()
+        loc = self.endElement()
+        self.assertRaises(web.NotFound, self.traverse, loc, 'TestLocation')
+
+    def testLocationId(self):
+        self.startElement('location',['id','root'])
+        loc = self.endElement()
+        self.failUnless(
+            self.traverse(loc,'++id++root').current is loc
+        )
+
+    def testOfferPath(self):
+        self.startElement('location',[])
+        self.startElement('offer',['path','foo', 'as','bar'])
+        self.endElement()
+        self.startElement('location',['name','foo'])
+        foo = self.endElement()
+        loc = self.endElement()
+        self.failUnless(
+            self.traverse(loc,'++id++bar').current is foo
+        )
+
+
+    def testRequirePermission(self):
+        self.startElement('location', [])
+
+        self.startElement('location',['name','foo'])
+        self.startElement('require',['permission','security.Anybody'])
+        self.startElement('container',['object','globals()',])
+        self.endElement('container')
+        self.endElement('require')
+        foo = self.endElement('location')
+        self.failUnless(
+            self.traverse(foo,'TestLocation').current is TestLocation
+        )
+
+        self.startElement('location',['name','bar'])
+        self.startElement('require',['permission','security.Nobody'])
+        self.startElement('container',['object','globals()',])
+        self.endElement('container')
+        self.endElement('require')
+        bar = self.endElement('location')
+        self.assertRaises(web.NotFound, self.traverse, bar, 'TestLocation')
+
+
+    # content(type) [location,helper,permission]
+    #   'offer' and 'container' should fail if inside 'content'
+
+    # view(name, resource|attribute|object|function)[helper,id,permission]
+
+    # allow(attributes|interfaces)[permission]
+
+    # location[include,configure]
+    # require[helper]
+
+    # XXX Location should support direct permissions, and ignore redundant ones
+
+
+class TestLocation(web.Location):
+    pass
+
 TestClasses = (
-    ParserTests, 
+    ParserTests,
 )
 
 def test_suite():
