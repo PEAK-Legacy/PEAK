3492e2393f0877001c971ab41317b790d3e7aa48
PJ Eby <pje@telecommunity.com>
2004-11-25 21:47:41 +0000

    Integrate peak.running.options w/peak.util.optparse.  Now we can parse options and show help, but several features aren't yet implemented.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@1949 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/running/options.py
+++ src/peak/running/options.py
@@ -27,12 +27,12 @@ def reject_inheritance(*names):
     addClassAdvisor(callback)
 
 
-def option_names(ob):
-    """List of option names that are applicable for 'ob'"""
-    data = OptionRegistry[ob,]
-    names = [k for k in data if data[k]<>(None,None)]
-    names.sort()
-    return names
+
+
+
+
+
+
 
 
 
@@ -42,8 +42,9 @@ def option_names(ob):
 class AbstractOption:
     """Base class for option metadata objects"""
 
-    sortKey = None
-    help = value = type = metavar = repeatable = option_names = NOT_GIVEN
+    repeatable = True
+    sortKey = metavar = help = None
+    value = type = option_names = NOT_GIVEN
 
     def __init__(self,*option_names,**kw):
         kw['option_names'] = option_names
@@ -65,12 +66,52 @@ class AbstractOption:
                 % self.__class__.__name__
             )
 
-        if self.type is NOT_GIVEN and self.metavar is not NOT_GIVEN:
+        if self.type is NOT_GIVEN and self.metavar is not None:
             raise TypeError(
                 "'metavar' is meaningless for options without a type"
             )
-        if self.metavar is NOT_GIVEN and self.type is not NOT_GIVEN:
-            self.metavar = self.type.__name__.upper()
+        if self.type is not NOT_GIVEN:
+            self.nargs = 1
+            if self.metavar is None:
+                self.metavar = self.type.__name__.upper()
+        else:
+            self.nargs = 0
+
+
+
+
+    def makeOption(self, attrname,optmap=None):
+        options = self.option_names
+
+        if optmap is not None:
+            options = [opt for opt in options if optmap.get(opt) is self]
+
+        from peak.util.optparse import make_option
+        return make_option(
+            action="callback", nargs=self.nargs, callback_args=(attrname,),
+            callback = self.callback, metavar=self.metavar, help=self.help,
+            type=(self.type is not NOT_GIVEN and "string" or None),*options
+        )
+
+    def convert(self,value):
+        if self.value is NOT_GIVEN:
+            return self.type(value)
+        else:
+            return self.value
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -82,19 +123,86 @@ class AbstractOption:
 
 class Set(AbstractOption):
     """Set the attribute to the argument value or a constant"""
+
     repeatable = False
+
+    def callback(self, option, opt, value, parser, attrname):
+        setattr(parser.values, attrname, self.convert(value))
+
 
 class Add(AbstractOption):
     """Add the argument value or a constant to the attribute"""
-    repeatable = True
+
+    def callback(self, option, opt, value, parser, attrname):
+        value = getattr(parser.values, attrname) + self.convert(value)
+        setattr(parser.values, attrname, value)
+
 
 class Append(AbstractOption):
     """Append the argument value or a constant to the attribute"""
-    repeatable = True
+
+    def callback(self, option, opt, value, parser, attrname):
+        getattr(parser.values, attrname, value).append(self.convert(value))
+
 
 class Handler(AbstractOption):
     """Invoke a handler method when the option appears on the command line"""
-    pass
+
+    def callback(self, option, opt, value, parser, attrname):
+        self.function(
+            parser.values, parser, opt, self.convert(value), parser.rargs
+        )
+
+
+
+
+
+
+
+
+
+
+def parse(ob,args):
+    parser = make_parser(ob)
+    opts, args = parser.parse_args(args, ob)
+    return args
+
+
+def exit_parser(status=0, msg=None):   
+    if msg:
+        from commands import InvocationError
+        raise InvocationError(msg.strip())
+    if status:
+        raise SystemExit(status)
+
+
+def make_parser(ob):
+    """Make an 'optparse.OptionParser' for 'ob'"""
+
+    from peak.util.optparse import OptionParser
+    parser = OptionParser("", add_help_option=False)
+    parser.exit = exit_parser
+
+    optinfo = OptionRegistry[ob,].items()
+    optmap = dict([(k,opt)for k,(a,opt) in optinfo if opt is not None])
+    optsused = {}
+    for optname,(attrname,option) in optinfo:
+        if option in optsused or option is None:
+            continue
+        parser.add_option( option.makeOption(attrname,optmap) )
+        optsused[option] = True
+
+    return parser
+
+def get_help(ob):
+    return make_parser(ob).format_help().strip()
+
+
+
+
+
+
+
 
 def option_handler(*option_names, **kw):
     """Decorate a method to be called when option is encountered"""
@@ -106,17 +214,33 @@ def option_handler(*option_names, **kw):
         return klass
 
     def decorator(frame,name,func,old_locals):
-        # option.function = func
+        option.function = func
         addClassAdvisor(class_callback,frame=frame)
         return func
 
     return add_assignment_advisor(decorator)
+
 
 [binding.declareAttribute.when(AbstractOption)]
 def declare_option(classobj,attrname,option):
     OptionRegistry[(classobj,)] = tuple(
         [(optname,(attrname,option)) for optname in option.option_names]
     )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
