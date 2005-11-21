8135c62bec837090028f2ef41e06ad676e38fd43
PJ Eby <pje@telecommunity.com>
2005-11-21 04:28:19 +0000

    Enhanced import algorithm, to work around issues with name collisions and to improve the available error messages, while keeping the same speed as the older algorithm in simple cases.
    git-svn-id: svn+ssh://svn.eby-sarna.com/svnroot/PEAK@2109 571e12c6-e1fa-0310-aee7-ff1267fa46bd

--- src/peak/util/imports.py
+++ src/peak/util/imports.py
@@ -5,7 +5,7 @@ __all__ = [
     'lazyModule', 'joinPath', 'whenImported', 'getModuleHooks',
 ]
 
-import __main__
+import __main__, sys
 defaultGlobalDict = __main__.__dict__
 
 from types import StringTypes, ModuleType
@@ -40,7 +40,6 @@ def joinPath(modname, relativePath):
     return '.'.join(module)
 
 def importString(name, globalDict=defaultGlobalDict):
-
     """Import an item specified by a string
 
     Example Usage::
@@ -61,24 +60,75 @@ def importString(name, globalDict=defaultGlobalDict):
     if ':' in name:
         name = name.replace(':','.')
 
-    path  = []
+    parts = filter(None,name.split('.'))
+    item = __import__(parts.pop(0), globalDict, globalDict, ['__name__'])
 
-    for part in filter(None,name.split('.')):
+    # Fast path for the common case, where everything is imported already
+    for attr in parts:
+        try:
+            item = getattr(item, attr)
+        except AttributeError:
+            break   # either there's an error, or something needs importing
+    else:
+        return item
 
-        if path:
-
+    # We couldn't get there with just getattrs from the base import.  So now
+    # we loop *backwards* trying to import longer names, then shorter, until
+    # we find the longest possible name that can be handled with __import__,
+    # then loop forward again with getattr.  This lets us give more meaningful
+    # error messages than if we only went forwards.
+    attrs = []
+    exc = None
+    
+    try:
+        while True:
             try:
-                item = getattr(item, part)
-                path.append(part)
-                continue
+                # Exit as soon as we find a prefix of the original `name`
+                # that's an importable *module* or package
+                item = __import__(name, globalDict, globalDict, ['__name__'])
+                break
+            except ImportError:
+                if not exc:
+                    # Save the first ImportError, as it's usually the most
+                    # informative, especially w/Python < 2.4
+                    exc = sys.exc_info()
 
-            except AttributeError:
-                pass
+                if '.' not in name:
+                    # We've backed up all the way to the beginning, so reraise
+                    # the first ImportError we got
+                    raise exc[0],exc[1],exc[2]
 
-        path.append(part)
-        item = __import__('.'.join(path), globalDict, globalDict, ['__name__'])
+                # Otherwise back up one position and try again
+                parts = name.split('.')
+                attrs.append(parts[-1])
+                name = '.'.join(parts[:-1])
+    finally:
+        exc = None
+
+    # Okay, the module object is now in 'item', so we can just loop forward
+    # to retrieving the desired attribute.
+    #
+    while attrs:
+        attr = attrs.pop()
+        try:
+            item = getattr(item,attr)
+        except AttributeError:
+            raise ImportError("%r has no %r attribute" % (item,attr))
 
     return item
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 def lazyModule(modname, relativePath=None):
 
